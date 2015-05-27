e0c2f639656774f30a1834ea53039198dc1e7ed6
Tyler Hudak <thudak@korelogic.com>
2015-05-27 14:16:39 -0400

    Updated version number (0.7.1.rc2 --> 0.7.1.sr0).

--- mastiff/__init__.py
+++ mastiff/__init__.py
@@ -23,7 +23,7 @@ This file contains package-level variables and functions.
 
 __version__ = "$Id$"
 
-version = 0x00701402
+version = 0x00701800
 
 def get_release_number():
     """ Gets the current release version. """
